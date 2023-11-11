ed392667dd57b8af62afd77d0d88331a86ecb9e2
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-11-11 20:52:20 +0530

    11-2nd

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(15)
+  function add()
+  {
+    if(counter<20){
+      counter =counter+1;
+      setCounter(counter)
+    }
+    
+  }
+  function sub()
+  {
+    if(counter >0){
+      counter = counter-1;
+      setCounter(counter)
+    }
+    
+  }
+  return (
+    <>
+      <h1>value: {counter}</h1>
+      <button onClick={add}> increment</button>
+      <br />
+      <button onClick={sub}> decrement</button>
+    </>
+  )
+}
+
+export default App
--- 02Counter/src/main.jsx
+++ 02Counter/src/main.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-const another = "chai aur node"
-
-const reactElement = React.createElement(
-  'a',
-  {href:'https://google.com', target: '_blank'},
-  another
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
