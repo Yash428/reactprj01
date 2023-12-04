ef1d941263765cd7a1cc0858bd16a939ab1a7eb4
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-12-04 11:25:35 +0530

    12-4-1st

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import Card from './components/Card'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-200 text-black p-4 rounded-xl'>tailwind test</h1>
+      <Card />
     </>
   )
 }
