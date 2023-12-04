ec4ab3b16fcb0d1d951005797c0db7f82f335701
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-12-04 12:54:02 +0530

    4-12-3rd

--- vitpr3/src/App.jsx
+++ vitpr3/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [color,setColor] = useState("olive")
 
   return (
-      <div className="h-screen w-full  duration-200 max-h-0" style={{backgroundColor:color}}>
+      <div className="w-full h-screen" style={{backgroundColor:color}}>
         <div className="fixed flex flex-wrap justify-center bottom-48 inset-x-0 px-2" >
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
             <button onClick={()=> setColor("red")} className="outline-none px-4 py-1 rounded-full text-white"
