a3e64b4b4988d66e0df3cfcfc5d45e66e4c8600f
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-12-15 18:48:41 +0530

    auth features added

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -17,8 +17,7 @@ export class AuthService{
         try{
             const userAccount =  await this.account.create(ID.unique(),email,password,name)
             if(userAccount){
-                //call another method
-                //return userAccount
+                return this.login(email,password)
             }
             else{
                 return userAccount;
@@ -27,6 +26,32 @@ export class AuthService{
             throw error;
         }
     }
+
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password)
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+
 }
 
 const authService = new AuthService();
