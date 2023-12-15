f72b767a9962f495752573a1d209dc4debddde77
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-12-15 17:46:20 +0530

    15-12-1st

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import conf from './conf/conf';
+
+function App() {
+console.log(conf.appwriteUrl);
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
