4be2e9f3af65d07fdebcbc074cfe340bdaaccb2a
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-12-20 14:00:55 +0530

    20-12-1st

--- 12MegaBlog/src/components/Button.jsx
+++ 12MegaBlog/src/components/Button.jsx
@@ -5,10 +5,11 @@ function Button({
     type = 'button',
     bgColor = 'bg-blue-600',
     textColor = 'text-white',
-    className = ''
+    className = '',
+    ...props
 }) {
   return (
-    <div>{children}</div>
+    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}` }{...props}>{children}</button>
   )
 }
 
